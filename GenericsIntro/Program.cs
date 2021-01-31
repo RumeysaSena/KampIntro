c66a277561d327972c648789ec0275bd215195b2
Rumeysa Sena Özdemir <77540535+ozrumsena@users.noreply.github.com>
2021-02-01 00:32:11 +0300

    4.gün

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Rumeysa");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Sena");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
