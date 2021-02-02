038c16f8172c9658570dbc94178aef8fd6e1b979
Rumeysa Sena Özdemir <77540535+ozrumsena@users.noreply.github.com>
2021-02-02 20:24:40 +0300

    Ödev 5

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    public class MyDictionary<TK, TV>
+    {
+        TK[] keys;
+        TV[] values;
+        public MyDictionary()
+        {
+            keys = new TK[0];
+            values = new TV[0];          
+        }
+
+        public void Add(TK key, TV value)
+        {
+            TK[] tempKey = keys;
+            TV[] tempValue = values;
+
+            keys = new TK[keys.Length + 1];
+            values = new TV[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+            values[values.Length - 1] = value;
+
+            Console.WriteLine("Plaka :" + key + "----" + "Sehir:" + value);
+        }
+          
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+
+            sehirler.Add(34, "İstanbul");
+            sehirler.Add(44, "Malatya");
+            sehirler.Add(23, "Elazığ");
+            sehirler.Add(06, "Ankara");
+            
+        }
+    }
+}
